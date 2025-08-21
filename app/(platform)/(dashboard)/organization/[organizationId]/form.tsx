325d315d04fefeeec4a7946fe139394fa54e0b51
niciyan <nishiyamasu@gmail.com>
2025-08-21 11:35:11 +0900

    finished server actions 3:51:58

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { useFormState } from "react-dom";
+import { FormInput } from "./form-input";
+import { FormButton } from "./form-button";
+
+const Form = () => {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(create, initialState);
+
+  return (
+    <form action={dispatch}>
+      <FormInput errors={state.errors} />
+      <FormButton />
+    </form>
+  );
+};
+
+export default Form;
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,11 +1,25 @@
+import { db } from "@/lib/db";
+import { DeleteBoard } from "./board";
+import Form from "./form";
+
 interface Props {
   params: {
     organizationId: string;
   };
 }
 
-const OrganizationIdPage = ({ params }: Props) => {
-  return <div>organization: {params.organizationId}</div>;
+const OrganizationIdPage = async ({ params }: Props) => {
+  const boards = await db.board.findMany();
+  return (
+    <div>
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <DeleteBoard title={board.title} id={board.id} key={board.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
