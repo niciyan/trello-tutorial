df3ace25f9a79705fff383548d81a72eac776721
niciyan <nishiyamasu@gmail.com>
2025-08-24 08:24:37 +0900

    finished board page 6:42:30

---
+++ actions/delete-board/types.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+import { Board } from "@/lib/generated/prisma";
+
+import { ActionState } from "@/lib/create-safe-action";
+import { DeleteBoard } from "./schema";
+
+export type InputType = z.infer<typeof DeleteBoard>;
+export type ReturnType = ActionState<InputType, Board>;
