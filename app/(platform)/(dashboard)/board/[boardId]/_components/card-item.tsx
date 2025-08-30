8a9251cceb22808ea902aafb5d39557d8d62e64a
niciyan <nishiyamasu@gmail.com>
2025-08-30 20:13:01 +0900

    finished drop and drop 9:06:44

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Card } from "@/lib/generated/prisma";
+import { Draggable } from "@hello-pangea/dnd";
 
 interface Props {
   data: Card;
@@ -9,11 +10,18 @@ interface Props {
 
 export const CardItem = ({ data, index }: Props) => {
   return (
-    <div
-      role="button"
-      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
-    >
-      {data.title}
-    </div>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   );
 };
